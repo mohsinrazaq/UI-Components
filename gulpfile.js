a578fbbf25298c888d31649b515a7b511717a165
Mohsin Razzaq <mohsinrazzaq25@gmail.com>
2018-02-21 00:05:10 +0500

    completed gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -7,3 +7,52 @@ const uglify = require('gulp-uglify');
 const sourcemaps = require('gulp-sourcemaps');
 const LessAutoprefix = require('less-plugin-autoprefix');
 const BrowserSync = require('browser-sync');
+
+var autoprefix = new LessAutoprefix({ browsers: ['last 2 versions'] });
+
+var lessDir = 'src/assets/less/';
+
+
+//Tasks: Minify and compile 
+
+gulp.task('html-minify',function(){
+    gulp.src('/src/*.html')
+        .pipe(htmlmin({collapsewhitespace:true}))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('image-minify', function(){
+    gulp.src('src/assets/images/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('dist/assets/images'));
+});
+
+gulp.task('less', function(){
+    gulp.src(lessDir + 'main.less')
+        .pipe(sourcemaps.init())
+        .pipe(less({
+            plugins: [autoprefix, require('less-plugin-glob')]
+        }))
+        .pipe(cleanCss())
+        .pipe(sourcemaps.write(/maps))
+        .pipe(gulp.dest('dist/assets/css/'))
+        .pipe(BrowserSync.stream());
+});
+
+
+//Tasks : Watc, Build and save
+
+gulp.task('serve', function(){
+    BrowserSync.init({
+        server: "dist"
+    });
+    gulp.watch('src/*.html', ['html-minify']);
+    gulp.watch('src/assets/image/*', ['image-minify']);
+    gulp.watch(lessDir + '**/*.less', ['less']);
+    gulp.watch('dist/*.html'.on('change', BrowserSync.reload));
+});
+
+
+//Tasks: Default Build Trigger 
+
+gulp.task('default',['html-minify', 'image-minify', 'less', 'serve']);
